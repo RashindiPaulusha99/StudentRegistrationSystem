5d7aa7468e276e36667da71526bf4a592e018251
RashindiPaulusha99 <rashindipaulusha837@gmail.com>
2021-12-19 00:26:17 +0530

    add register form

---
+++ src/controller/ForgotPasswordFormController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class ForgotPasswordFormController {
+
+    public AnchorPane forgotPasswordContext;
+    public TextField txtUsername;
+    public PasswordField txtPassword;
+    public PasswordField txtConfirmPassword;
+
+    public void loginFormOnAction(ActionEvent event) {
+
+    }
+
+    public void registerFormOnAction(ActionEvent event) {
+
+    }
+
+    public void resetOnAction(ActionEvent event) {
+
+    }
+}
---
+++ src/controller/RegisterFormController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class RegisterFormController {
+
+    public AnchorPane RegisterContext;
+    public TextField txtUsername;
+    public TextField txtPassword;
+    public TextField txtUserId;
+
+    public void loginFormOnAction(ActionEvent event) {
+
+    }
+
+    public void forgotPasswordFormOnAction(ActionEvent event) {
+
+    }
+
+    public void registerOnAction(ActionEvent event) {
+
+    }
+}
