936ed1a1dafc04e9042ae26dcdb53ebe2ccfccdc
RashindiPaulusha99 <rashindipaulusha837@gmail.com>
2021-12-18 21:18:01 +0530

    add login form

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,5 +1,24 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class LoginFormController {
 
+    public AnchorPane loginContext;
+    public TextField txtUsername;
+    public TextField txtPassword;
+
+    public void DashBoardFormOnAction(ActionEvent event) {
+
+    }
+
+    public void registerFormOnAction(ActionEvent event) {
+
+    }
+
+    public void forgotPasswordFormOnAction(ActionEvent event) {
+
+    }
 }
