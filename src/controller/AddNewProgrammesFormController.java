a304b7927d61c885fb30b678189cc0806e7490ab
RashindiPaulusha99 <rashindipaulusha837@gmail.com>
2021-12-25 20:49:18 +0530

    create add new programme form

--- src/controller/AddNewProgrammesFormController.java
+++ src/controller/AddNewProgrammesFormController.java
@@ -1,4 +1,27 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class AddNewProgrammesFormController {
+
+    public TextField txtPID;
+    public TextField txtPName;
+    public TextField txtFee;
+    public ComboBox cmbDuration;
+    public TableView tblProgrammes;
+
+    public void addNewCourseOnAction(ActionEvent event) {
+
+    }
+
+    public void addCourseOnAction(ActionEvent event) {
+
+    }
+
+    public void clearCourseOnAction(ActionEvent event) {
+
+    }
 }
